b3416fdb86dd718f3ae5902cfe0de78c6838c943
hyrious <hyrious@outlook.com>
2021-02-21 17:13:26 +0800

    fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import preactRefresh from "@prefresh/vite";
 
 export default defineConfig({
+    base: "./",
     esbuild: {
         jsxFactory: "h",
         jsxFragment: "Fragment",
